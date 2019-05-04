058271e4d06427c3f442a0eb4c22ae0e7e5e80dd
Andreas Dominik <andreas.dominik@mni.thm.de>
2019-05-04 11:33:15 +0200

    all devices "not handled as default"

--- SnipsHermesQnD/src/gpio.jl
+++ SnipsHermesQnD/src/gpio.jl
@@ -7,47 +7,21 @@
 #
 
 """
-function setGPIOtoON(name)
-
-    Switches GPIO on.
-
-# Arguments:
-    * gpio: Name of GPIO to be switched as defind in config.ini.
-      config.ini must has an entry like:
-      GPIOtvLivingroom=24
-"""
-function setGPIOtoON(name)
-
-    return setGPIO( name, 1)
-end
-
-
-"""
-function setGPIOtoOFF(name)
-
-    Switches GPIO off.
-
-# Arguments:
-        * gpio: Name of GPIO to be switched as defind in config.ini.
-          config.ini must has an entry like:
-          GPIOtvLivingroom=24
-"""
-function setGPIOtoOFF(name)
-
-    return setGPIO( name, 0)
-end
-
-
-"""
-function setGPIO(name, onoff)
+function setGPIO(gpio, onoff::Symbol)
 
     Switches GPIO on or off.
-"""
-function setGPIO(name, onoff)
 
-    gpio = getConfig(name)
-    if gpio != nothing
-        shell = `pigs w $gpio $onoff`
+# Arguments:
+    * gpio: ID of GPIO (not pinID)
+    * onoff: one of :on or :off
+"""
+function setGPIO(gpio, onoff::Symbol)
+
+    if gpio == :on
+        shell = `pigs w $gpio 1`
+        tryrun(shell, errorMsg = TEXTS[:error_gpio])
+    elseif gpio == :off
+        shell = `pigs w $gpio 0`
         tryrun(shell, errorMsg = TEXTS[:error_gpio])
     end
 end
