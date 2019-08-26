95a837c09d44f7aa7186b49c493360026ac73b8e
andreas.dominik <andreas.dominik@mni.thm.de>
2019-08-26 12:43:43 +0200

    addAction and isDue()

--- QnDintents/api.jl
+++ QnDintents/api.jl
@@ -3,6 +3,33 @@
 # skill-actions:
 #
 
+# Scheduler functions:
+#
+#
+# The db looks like:
+# [
+#   {
+#     "create_time" : "2019-08-25T10:01:35.399"
+#     "execute_time" : "2019-08-26T10:12:13.177"
+#     "topic" : "qnd/trigger/andreasdominik:ADoSnipsLights",
+#     "trigger" :
+#     {
+#       "room" : "default",
+#       "device" : "floor_lamp",
+#       "onOrOff" : "ON",
+#       "settings" : "undefined"
+#     }
+#   },
+#   {
+#     ...
+#   }
+# ]
+#
+# The db is always sorted; i.e. the entry with the oldest scheduled
+# execution time is first.
+#
+
+
 function readScheduleDb()
 
     db = Snips.dbReadValue(:scheduler, :db)
@@ -10,5 +37,26 @@ function readScheduleDb()
         return Dict()
     else
         return db
+    end
+end
+
+function addAction!(db, action)
+
+    push!(db, action)
+    sort!(db, by = x->x[:execute_time])
+end
+
+"""
+    function isDue(action)
+
+Check, if the scheduled execution stime of action is in the past
+and return `true` or `false` if not.
+"""
+function isDue(action)
+
+    if haskey(action, :exec_time)
+        return DateTime(action) < Dates.now()
+    else
+        return false
     end
 end
