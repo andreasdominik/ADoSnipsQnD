5b97741ae3655944576211e528fe9ad4bedc1069
andreas.dominik <andreas.dominik@mni.thm.de>
2020-03-22 09:12:50 +0100

    languages: new method

--- QnDintents/languages.jl
+++ QnDintents/languages.jl
@@ -6,5 +6,5 @@ TEXTS_EN = Dict(
 :not_handled => " I am not able to control this device!"
 )
 
-Snips.addText("de", :not_handled => "Das Gerät kann ich nicht steuern!")
-Snips.addText("en", :not_handled => "I am not able to control this device!")
+Snips.addText("de", :not_handled, "Das Gerät kann ich nicht steuern!")
+Snips.addText("en", :not_handled, "I am not able to control this device!")
